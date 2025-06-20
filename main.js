d648dff34f25bd055e1542c3e063b23f5ae7e26b
fluxicityx <126483986+fluxicityx@users.noreply.github.com>
2025-06-19 21:03:05 -0400

    calculate function now allows you to add numbers

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ let operator = "";
 const currentDisplayNumber = document.querySelector(".currentNumber");
 const previousDisplayNumber = document.querySelector(".previousNumber");
 
-const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear")
 
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", calculate)
 const decimal = document.querySelector(".clear");
 
 const numberButtons = document.querySelectorAll(".number")
@@ -34,7 +37,17 @@ operators.forEach(btn => {
 function handleOperator(op){
     operator = op;
     previousNum = currentNum
-    previousDisplayNumber.textContent = previousNum
+    previousDisplayNumber.textContent = previousNum + " " + operator;
     currentNum = ""
     currentDisplayNumber.textContent ="";
+}
+
+function calculate(){
+    previousNum  = Number(previousNum)
+    currentNum  = Number(currentNum)
+    if(operator ==="+"){
+        previousNum = previousNum + currentNum;
+    }
+    previousDisplayNumber.textContent = ""
+    currentDisplayNumber.textContent = previousNum;
 }
